a7e4b02dba17c5361b14a7dfcbcd810c8dadf56c
spaciuk <sebapaciuk@gmail.com>
2016-06-22 22:37:45 -0300

    Agregado estilos, validaciones, etc en los formularios

--- responses.js
+++ responses.js
@@ -1,76 +1,94 @@
 var assert = require("assert");
+require("date-utils");
 
 module.exports.getNews = function(db, response){
-    var result = [];
-    var cursor = db.collection("news").find().limit(10);
-    response.writeHead(200, {"Content-Type": "application/json"});
-    cursor.each(function(err, doc) {
-        assert.equal(err, null);
-        if (doc != null) {
-            result.push(doc);
-        } else {
-            var string = JSON.stringify(result);
-            response.write(string);
-            response.end();
-        }
-    });
+  var result = [];
+  var cursor = db.collection("news").find().limit(10);
+  response.writeHead(200, {"Content-Type": "application/json"});
+  cursor.each(function(err, doc) {
+    assert.equal(err, null);
+    if (doc != null) {
+      result.push(doc);
+    } else {
+      var string = JSON.stringify(result);
+      response.write(string);
+      response.end();
+    }
+  });
+}
+
+module.exports.getNewsByChannels = function(db, canales, response){
+  var result = [];
+  var cursor = db.collection("news").find( { canal: { $in: canales } } ).sortBy("fecha");
+  response.writeHead(200, {"Content-Type": "application/json"});
+  cursor.each(function(err, doc) {
+    assert.equal(err, null);
+    if (doc != null) {
+      result.push(doc);
+    } else {
+      var string = JSON.stringify(result);
+      response.write(string);
+      response.end();
+    }
+  });
 }
 
 module.exports.getChannels = function(db, response){
-    var result = [];
-    var cursor = db.collection("channels").find();
-    response.writeHead(200, {"Content-Type": "application/json"});
-    cursor.each(function(err, doc) {
-        assert.equal(err, null);
-        if (doc != null) {
-            result.push(doc);
-        } else {
-            var string = JSON.stringify(result);
-            response.write(string);
-            response.end();
-        }
-    });
+  var result = [];
+  var cursor = db.collection("channels").find();
+  response.writeHead(200, {"Content-Type": "application/json"});
+  cursor.each(function(err, doc) {
+    assert.equal(err, null);
+    if (doc != null) {
+      result.push(doc);
+    } else {
+      var string = JSON.stringify(result);
+      response.write(string);
+      response.end();
+    }
+  });
 }
 
 module.exports.addNew = function(db, noticia,response){
-    db.collection("news").insertOne( {
-        "titulo": noticia.titulo,
-        "autor": noticia.autor,
-        "canal": noticia.canal,
-        "contenido": noticia.contenido,
-        "imagen": noticia.imagen
-    }, function(err, result) {
-        assert.equal(err, null);
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("Noticia Insertada!");
-        response.end();
-        //TODO: Migrar a FireBase
-        //notification.send("/topics/"+params.topic, params.title, params.body);
-    });
+  db.collection("news").insertOne( {
+    "titulo": noticia.titulo,
+    "autor": noticia.autor,
+    "canal": noticia.canal,
+    "contenido": noticia.contenido,
+    "imagen": noticia.imagen,
+    "fecha": Date().toString()
+  }, function(err, result) {
+    assert.equal(err, null);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Noticia Insertada!");
+    response.end();
+    //TODO: Migrar a FireBase
+    //notification.send("/topics/"+params.topic, params.title, params.body);
+  });
 }
 
 module.exports.addEvent = function(db, evento,response){
-    db.collection("events").insertOne( {
-        "fecha": evento.fecha,
-        "titulo": evento.titulo,
-        "lugar": evento.lugar
-    }, function(err, result) {
-        assert.equal(err, null);
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("Evento Insertado!");
-        response.end();
-    });
+  db.collection("events").insertOne( {
+    "fecha": evento.fecha,
+    "titulo": evento.titulo,
+    "lugar": evento.lugar
+  }, function(err, result) {
+    assert.equal(err, null);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Evento Insertado!");
+    response.end();
+  });
 }
 
 module.exports.addChannel = function(db, canal,response){
-    db.collection("channels").insertOne( {
-        "name": canal.name,
-        "id": canal.id,
-        "description": canal.description
-    }, function(err, result) {
-        assert.equal(err, null);
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("Canal Insertado!");
-        response.end();
-    });
+  db.collection("channels").insertOne( {
+    "id": canal.id,
+    "name": canal.name,
+    "description": canal.description
+  }, function(err, result) {
+    assert.equal(err, null);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Canal Insertado!");
+    response.end();
+  });
 }
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-var PORT = 80;
-
+var PORT = 3000;
 var express = require("express");
 var auth = require('http-auth');
 var basic = auth.basic({
@@ -14,54 +13,70 @@ var db;
 
 var MongoClient = require("mongodb").MongoClient;
 var db = MongoClient.connect("mongodb://127.0.0.1:27017/local", function(err, database) {
-    if(err){
-        //throw err;
-        console.log("Database connection failed.");
-    } else{
-        db = database;
-        console.log("Connected to Database!");
-    }
-    initServer();
+	if(err){
+		//throw err;
+		console.log("Database connection failed.");
+	} else{
+		db = database;
+		console.log("Connected to Database!");
+	}
+	initServer();
 });
 
 function initServer(){
-    var app = express();
-	  var path = __dirname + "/public";
+	var app = express();
+	var path = __dirname + "/public";
 
-    app.use(bodyParser.json()); // to support JSON-encoded bodies
-    app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-      extended: true
-    }));
+	app.use(bodyParser.json()); // to support JSON-encoded bodies
+	app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+		extended: true
+	}));
 
-    app.post('/addNew', auth.connect(basic), function(req, res){
-        var params = req.body;
-        var noticia = {title: params.titulo, autor: params.autor, canal: params.canal, contenido: params.contenido, imagen: params.imagen};
-        console.log(params.titulo, params.autor, params.canal, params.contenido, params.imagen);
-        query.addNew(db, noticia, res);
-    });
+	app.post('/addNew', auth.connect(basic), function(req, res){
+		var params = req.body;
+		var noticia = {titulo: params.titulo, autor: params.autor, canal: params.canal, contenido: params.contenido, imagen: params.imagen};
+		query.addNew(db, noticia, res);
+	});
 
-		app.post('/addEvent', auth.connect(basic), function(req, res){
-        var params = req.body;
-        var evento = {fecha: params.fecha, titulo: params.titulo, lugar: params.lugar};
-        console.log(params.fecha, params.titulo, params.lugar);
-        query.addEvent(db, evento, res);
-    });
+	app.post('/addEvent', auth.connect(basic), function(req, res){
+		var params = req.body;
+		var evento = {titulo: params.titulo, lugar: params.lugar, canal: params.canal, fecha: params.fecha};
+		query.addEvent(db, evento, res);
+	});
 
-		app.post('/login', auth.connect(basic), function(req, res){
-				var params = req.body;
-        var cred = {usuario: params.inputUser, contraseña: params.inputPassword};
-        console.log(cred);
-				//si todo OK, devolvemos el form de noticia y evento :)
-				res.redirect("http://localhost/noticias.html");
+	app.post('/login', auth.connect(basic), function(req, res){
+		var params = req.body;
+		var cred = {usuario: params.inputUser, contraseña: params.inputPassword};
+		console.log(cred);
+		//si todo OK, devolvemos el form de noticia y evento :)
+		fs.readFile(path + "/noticias.html", function(err,data){
+			if(err){throw err;}
+			res.writeHead(200,{
+				"Content-Type" : "text/html",
+				"Content-length" : data.length
 			});
+			res.write(data);
+			res.end();
+		});
+	});
 
-    app.use('/', express.static(path));
+	app.get('/getNews', function(req, res){
+		query.getNews(db, res);
+	});
 
-    app.get('/news', function(req, res){
-        query.getNews(db, res);
-    });
+	app.get('/getChannels', function(req, res){
+		query.getChannels(db, res);
+	});
 
-    app.listen(PORT, function () {
-        console.log('Server running on port '+PORT);
-    });
+	app.get('/getNewsByChannels',function(req,res){
+		var params = req.body;
+		canales = params.canales;
+		query.getNewsByChannels(db, canales, res);
+	});
+
+	app.use('/', express.static(path));
+
+	app.listen(PORT, function () {
+		console.log('Server running on port '+PORT);
+	});
 }
